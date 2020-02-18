056e1d4ed87e7686d014ad0de6788a12c49bda96
Xavi Pedrals <xavi.pedrals@themobilecompany.com>
2020-02-18 08:34:45 +0100

    Initial Commit

---
+++ TrafficPins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficPins
+//
+//  Created by Xavi Pedrals | The Mobile Company on 18/02/2020.
+//  Copyright © 2020 xavi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
